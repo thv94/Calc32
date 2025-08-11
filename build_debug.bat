ab54c86d272d4e9aa7cffc9d2775e0573e7cd3d3
Taylor Vaughn <78456999+thv94@users.noreply.github.com>
2025-08-10 22:17:42 -0400

    Updates to build scripts

--- build_debug.bat
+++ build_debug.bat
@@ -6,6 +6,10 @@ IF NOT EXIST build\debug mkdir build\debug
 
 pushd build\debug
 
-call cl -nologo /EHa- /GR- /Oi /TC /WX /W4 /Z7 /FC /Od /I"C:\Users\user\MyStuff\Code\Calc32\include" ..\..\src\calc.c ..\..\src\computations.c /link user32.lib gdi32.lib
+call cl -MT -nologo /EHa- /GR- /Oi /TC /WX /W4 /Z7 /FC /Od /I"C:\Users\user\MyStuff\Code\Calc32\include" ..\..\src\calc.c ..\..\src\computations.c /link user32.lib gdi32.lib
+
+if %ERRORLEVEL% == 0 (
+    start calc.exe
+)
 
 popd
--- build_release.bat
+++ build_release.bat
@@ -6,6 +6,10 @@ IF NOT EXIST build\release mkdir build\release
 
 pushd build\release
 
-call cl -nologo /EHa- /GR- /Oi /TC /WX /W4 /FC /O2 /I"C:\Users\user\MyStuff\Code\Calc32\include" ..\..\src\calc.c ..\..\src\computations.c /link user32.lib gdi32.lib
+call cl -MT -nologo /EHa- /GR- /Oi /TC /WX /W4 /FC /O2 /I"C:\Users\user\MyStuff\Code\Calc32\include" ..\..\src\calc.c ..\..\src\computations.c /link user32.lib gdi32.lib
+
+if %ERRORLEVEL% == 0 (
+    start calc.exe
+)
 
 popd
